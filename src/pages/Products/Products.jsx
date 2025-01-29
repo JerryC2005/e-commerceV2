806ec9b64e11adc1024cc8feebf045008990a178
JerryC2005 <jerrycastroluz@gmail.com>
2025-01-28 22:31:14 -0500

    bruhojsdn

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,45 +1,38 @@
-
 import { useEffect, useState } from "react";
 
 export default function Products() {
-    const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
 
-    useEffect(() => {
-        fetch('https://e-commerce-v2-up2k.vercel.app/api/products')
-        .then(response => response.json())
-        .then(data => {
-            setProducts(data);
-        })
-    }, []);
+  useEffect(() => {
+    fetch("https://e-commerce-v2-up2k.vercel.app/api/products")
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts(data);
+      });
+  }, []);
 
-    
-    
+  return (
+    <>
+      <header className="product-header">
+        <h2>Products</h2>
+      </header>
 
-    return (
-        <>
-        <header className="product-header">
-            <h2>Products</h2>
-        </header>
-
-        
-
-        <div className="container">
-            <section className="product-grid">
-                {products.map((product, index) => {
-                    return (
-                        <div className="product-card" key={index}>
-                            <img src={product.img} alt={product.name}/>
-                            <section className="product-section">
-                                <h3 className="name">{product.name}</h3>
-                                <p className="description">{product.description}</p>
-                                <p className="price">${product.price}</p>
-                            </section>
-                        </div>
-                    )
-                })}
-                
-            </section>
-        </div>
-        </>
-    )
+      <div className="container">
+        <section className="product-grid">
+          {products.map((product, index) => {
+            return (
+              <div className="product-card" key={index}>
+                <img src={product.img} alt={product.name} />
+                <section className="product-section">
+                  <h3 className="name">{product.name}</h3>
+                  <p className="description">{product.description}</p>
+                  <p className="price">${product.price}</p>
+                </section>
+              </div>
+            );
+          })}
+        </section>
+      </div>
+    </>
+  );
 }

f161f276c64bdcc83b3c91a2921c0ca51413316c
JerryC2005 <jerrycastroluz@gmail.com>
2025-01-28 23:07:55 -0500

    vercel

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,11 +3,11 @@ import express from "express";
 import displayProducts from "./database/databaseQueries.js";
 import dotenv from "dotenv";
 import cors from "cors";
+
 dotenv.config();
 
-
 //Create backend server w/ express
-const app = express()
+const app = express();
 
 app.use(cors());
 
@@ -15,18 +15,18 @@ app.use(cors());
 app.use(express.json());
 
 // route to get product from api
-app.get('/api/products', async (req, res) => {
-    try {
-        const products = await displayProducts(); // Fetch products
-        res.send(products); // Send the products as a obj response
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Internal Server Error" });
-    }
-})
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await displayProducts(); // Fetch products
+    res.send(products); // Send the products as a obj response
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
 
 //Hosts the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+  console.log(`Server is running on port ${port}`);
+});
